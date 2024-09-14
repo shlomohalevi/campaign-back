b295efbcb867d6246185bd3021f44a628916d4db
shlomohalevi <shlomoww@gmail.com>
2024-09-15 01:54:20 +0300

    update

--- Routes/commitmentRoute.js
+++ Routes/commitmentRoute.js
@@ -7,7 +7,7 @@ router.route('/upload').post(commitmentController.uploadCommitment)
 router.route('/uploadPayment').post(commitmentController.uploadPayment);
 router.route('/uploadCommitmentPayment').post(commitmentController.uploadCommitmentPayment);
 router.route('/get-commitment/:_id').get(commitmentController.getCommitmentById)
-router.route('/getcommitmentbyanashandcampaign/:AnashIdentifier/:CampainName').get(commitmentController.getcommitmentbyanashandcampaign);
+router.route('/get-commitment-by-anash-and-campain').get(commitmentController.getcommitmentbyanashandcampaign);
 router.route('/delete-commitment/:commitmentId').delete(commitmentController.deleteCommitment)
 router.route('/update-commitment-details/:commitmentId').post(commitmentController.updateCommitmentDetails);
 
