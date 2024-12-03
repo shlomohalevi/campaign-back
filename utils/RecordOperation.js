6f142d3b0e90d6d5907b063b034b3ab392a37abc
shlomohalevi <shlomoww@gmail.com>
2024-12-03 02:30:36 +0200

    u

--- utils/RecordOperation.js
+++ utils/RecordOperation.js
@@ -9,33 +9,57 @@ const peopleModel = require('../models/peopleModel')
 
 
 
-exports.recordEditOperation =  (operation) => {
-    const newValues = operation.NewValues
-    const oldValues = operation.OldValues
+exports.recordEditOperation = (operation) => {
+    const { NewValues, OldValues, Date, OperationType, UserFullName, Desc } = operation;
     const newOperation = {
-        Date: operation.Date,
-        OperationType: operation.OperationType,
-        UserFullName: operation.UserFullName,
-        Data:{
-            OldValue: {},
-            NewValue: {}}
-            ,
-        Desc: operation.Desc
-        }
-    for (const [key, value] of Object.entries(newValues)) {
-        if(oldValues[key] !=value) {
-            newOperation.Data.NewValue[key] = value
-            newOperation.Data.OldValue[key] = oldValues[key]
-        }
+      Date,
+      OperationType,
+      UserFullName,
+      Data: { OldValue: {}, NewValue: {} },
+      Desc,
+    };
+  
+    for (const [key, value] of Object.entries(NewValues)) {
+      if (!Array.isArray(value)) {
+        compareNonArrayValues(key, value, OldValues, newOperation);
+      } else if (key === 'MemorialDays') {
+        compareArrayValues(key, value, OldValues, newOperation);
+      }
     }
-    if(Object.keys(newOperation.Data.NewValue).length  === 0) {
-        return null
-    }
+  
+    return Object.keys(newOperation.Data.NewValue).length === 0 ? null : newOperation;
+  };
+  
 
-    else{
-        return newOperation
+function compareNonArrayValues(key, value, oldValues, newOperation) {
+    if (oldValues[key] != value) {
+      newOperation.Data.NewValue[key] = value;
+      newOperation.Data.OldValue[key] = oldValues[key];
     }
-}
+  }
+  
+  function compareArrayValues(key, value, oldValues, newOperation) {
+    if (value.length !== oldValues[key].length) {
+      newOperation.Data.NewValue[key] = value;
+      newOperation.Data.OldValue[key] = oldValues[key];
+    } else {
+      for (let i = 0; i < value.length; i++) {
+        if (!isTheSameDate(new Date(value[i].date), new Date(oldValues[key][i].date))) {
+          newOperation.Data.NewValue[key] = value;
+          newOperation.Data.OldValue[key] = oldValues[key];
+          break;
+        }
+      }
+    }
+  }
+  
+function isTheSameDate(date1, date2) {
+    return (
+      date1.getFullYear() === date2.getFullYear() &&
+      date1.getMonth() === date2.getMonth() &&
+      date1.getDate() === date2.getDate()
+    );
+  }
 
 exports.recordDeleteOperation =  (operation) => {
     const newOperation = {
