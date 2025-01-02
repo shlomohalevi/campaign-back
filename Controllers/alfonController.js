82646f2d3340e830aedf7e787d0182570046bb6a
shlomohalevi <shlomoww@gmail.com>
2025-01-02 12:51:19 +0200

    update

--- Controllers/alfonController.js
+++ Controllers/alfonController.js
@@ -78,7 +78,6 @@ exports.uploadPeople = asyncHandler(async (req, res, next) => {
 
 exports.reviewUploadedPeople = asyncHandler(async (req, res, next) => {
   const reviewedPeople = req.body;
-  console.log(specificPerson);
   const validPeople = [];
   const invalidPeople = [];
   const conflictedPeople = [];
